4629fab82599cc749cdb7502eeaf46be2a760e09
张嘉明 <Zhangjiamingitisme@outlook.com>
2017-07-10 12:04:03 +0800

    Status: Accepted
    You are here! 
    Your runtime beats 54.68 % of cpp submissions.
    使用unordered_map处理

---
+++ CPP/1.Two Sum/2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> hash;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); ++i) {
+			int numToFind = target - nums[i];
+			if (hash.find(numToFind) != hash.end()) {
+				result.push_back(hash[numToFind]);
+				result.push_back(i);
+				return result;
+			}
+			else {
+				hash[nums[i]] = i;
+			}
+		}
+		return result;
+	}
+};
+	
